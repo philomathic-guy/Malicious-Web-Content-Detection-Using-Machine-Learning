3d52c68ea6e093d584a75abf75c0d9fe6587189d
rohit <rohit.naik246@gmail.com>
2017-01-13 16:56:03 +0530

    url.py 1st day - Final

--- url.py
+++ url.py
@@ -5,7 +5,7 @@
 import re
 
 def having_ip_address(url):
-    match=re.search('^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$|0x[0-9A-F]{1,4}\.0x[0-9A-F]{1,4}\.0x[0-9A-F]{1,4}\.0x[0-9A-F]{1,4}',url)
+    match=re.search('(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5]))|(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}',url)
     #IP address outside the permissible range condition not added yet. Eg. 999.400.500.260 will also be an IP address
     #If more than 3 digits in IP address, then it takes only 3 digits and proceeds. BUT IT SHOULD SHOW NOT A MATCH. CHECK!
     if match:
@@ -23,9 +23,13 @@ def url_length(url):
     else:
         return -1
 
-#REMAINING
-def shortened(url):
-    match=re.search('',url)
+def shortened_url(url):
+#These form the maximum used URL shortening serivices. If possible, find more
+    match=re.search('bit\.ly|goo\.gl|ow\.ly|t\.co|tinyurl|tr\.im',url)
+    if match:
+        return -1
+    else:
+        return 1
 
 def having_at_symbol(url):
     match=re.search('@',url)
@@ -52,8 +56,10 @@ def having_dash_symbol(url):
 def subdomains(url):
 #Here, instead of greater than 1 we will take greater than 3 since the greater than 1 conition is when www and country domain dots are skipped
 #Accordingly other dots will increase by 1
-
-#SHOULD NOT INCLUDE DOTS IN IP ADDRESS
+    if having_ip_address(url)==-1:
+        match=re.search('(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5]))|(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}',url)
+        pos=match.end(0)
+        url=url[pos:]
     list=[x.start(0) for x in re.finditer('\.',url)]
     if len(list)<=3:
         return 1
@@ -61,8 +67,6 @@ def subdomains(url):
         return 0
     else:
         return -1
-
-#def
 
 def main():
     status=[]
@@ -73,7 +77,8 @@ def main():
     status.append(having_doubleslash(url))
     status.append(having_dash_symbol(url))
     status.append(subdomains(url))
-    print '1. Having IP address\n2. URL Length\n3. Having @ symbol\n4. Having double slash\n5. Having dash symbol\n6. Having multiple subdomains'
+    status.append(shortened_url(url))
+    print '\n1. Having IP address\n2. URL Length\n3. Having @ symbol\n4. Having double slash\n5. Having dash symbol\n6. Having multiple subdomains\n7. URL Shortened\n'
     print status
 
 if __name__ == "__main__":
