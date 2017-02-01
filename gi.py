05917216eb891b8efa6bf769939f354360df7e15
rohit <rohit.naik246@gmail.com>
2017-02-01 10:39:02 +0530

    phantomJS code added in html_js.py file. on_mouseover, right click, and pop up window to be added here

--- gi.py
+++
@@ -1,4 +0,0 @@
-from google import search
-
-for url in search('"Breaking Code" Wordpress blog',stop=10):
-	print(url)
--- html_js.py
+++ html_js.py
@@ -1,3 +1,4 @@
+from selenium import webdriver
 import urllib2, httplib
 
 #condition for more than 1 redirect is left. Looking for a url which redirects more than once.
@@ -9,9 +10,18 @@ def redirect(url):
     print f.url
     return 1
 
+def on_mouseover(page_source_with_js):
+    print 'HI'
+
 def main():
-    status=[]
-    url='http://ow.ly/gxoS308a1EM'
+    status = []
+    url = 'http://ow.ly/gxoS308a1EM'
+
+    driver = webdriver.PhantomJS()
+    driver.get(url)
+    page_source_with_js=driver.page_source
+
+    status.append(on_mouseover(page_source_with_js))
     status.append(redirect(url))
 
 if __name__ == "__main__":
