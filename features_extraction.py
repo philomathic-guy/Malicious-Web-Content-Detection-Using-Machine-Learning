a96293e02784760329a652ef942b919e0970cf30
rohit <rohit.naik246@gmail.com>
2017-02-03 17:14:42 +0530

    Having IP address updated

--- features_extraction.py
+++ features_extraction.py
@@ -8,12 +8,12 @@ import re, urllib2, httplib
 import OpenSSL, ssl
 import urllib, sys, bs4
 
-#from google import search
+from google import google
 
 def having_ip_address(url):
-    match=re.search('(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5]))|(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}',url)
-######IP address outside the permissible range condition not added yet. Eg. 999.400.500.260 will also be an IP address######
-######If more than 3 digits in IP address, then it takes only 3 digits and proceeds. BUT IT SHOULD SHOW NOT A MATCH. CHECK!######
+    match=re.search('(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\/)|'  #IPv4
+                    '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)'  #IPv4 in hexadecimal
+                    '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}',url)     #Ipv6
     if match:
         #print match.group()
         return -1
@@ -29,16 +29,16 @@ def url_length(url):
     else:
         return -1
 
-
 def shortening_service(url):
     match=re.search('bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
-                    'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu||twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
+                    'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
                     'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
                     'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
                     'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
                     'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
                     'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|tr\.im|link\.zip\.net',url)
     if match:
+        print match.group()
         return -1
     else:
         return 1
@@ -220,7 +220,6 @@ def url_of_anchor(wiki, soup):
         return 1
 
 # Links in <Script> and <Link> tags
-###### <Meta> has no links in most of the websites..so can't compare with domain names ######
 def links_in_tags(wiki, soup):
    i=0
    success =0
@@ -285,8 +284,8 @@ def iframe(soup):
         if iframe['width']=="0" and iframe['height']=="0" and iframe['frameBorder']=="0":
             return -1
         else:
-            return 0
-    return 0
+            return 1
+    return 1
 
 ###### AGE OF DOMAIN ######
 
@@ -304,12 +303,11 @@ def web_traffic(url):
         return 0
 
 def google_index(url):
-    return 1
-    # site=search(url, stop=5)
-    # if site:
-    #     return 1
-    # else:
-    #     return -1
+    site=google.search(url, 5)
+    if site:
+        return 1
+    else:
+        return -1
 
 ##### LINKS PONITING TO PAGE #####
 
