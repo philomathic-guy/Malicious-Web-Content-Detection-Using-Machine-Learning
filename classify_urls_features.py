74351665edca59cc55585cd7e41309d91c7c9010
Andy71195 <ananddesai71195@gmail.com>
2017-02-10 11:45:01 +0530

    FInalising the Algorithm with Random Forest(split=7)

--- classify_urls_features.py
+++ classify_urls_features.py
@@ -9,10 +9,13 @@ from sklearn import svm
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
+from sklearn import preprocessing
+from sklearn import metrics
 
 labels=[]
 features=[]
-file=open('/home/janvi-jatakia/Downloads/Training.arff').read()
+file=open('/home/anand/PycharmProjects/BE/Training Dataset.arff').read()
 list=file.split('\r\n')
 #f=open('/home/janvi-jatakia/Downloads/data.txt','w')
 data=np.array(list)
@@ -22,7 +25,7 @@ for i in data1:
 	labels.append(i[30])
 data1=np.array(data1)
 features=data1[:,:-1]
-features=features[:,[0,1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,18,19,20,21,22,25,26,27]]
+features=features[:,[0,1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,22,23,24,25,27,29]]
 #print features
 features=np.array(features).astype(np.float)
 
@@ -32,29 +35,39 @@ features_test=features[10000:]
 labels_test=labels[10000:]
 
 print(" ""Support Vector Machine Results"" ")
-clf1 = svm.SVC(kernel="rbf",C=10000.0,gamma=0.1,probability=True)
+clf1 = svm.SVC(probability=True)
 clf1.fit(features_train,labels_train)
 pred1=clf1.predict(features_test)
-#print(classification_report(labels_test, pred1))
+print(classification_report(labels_test, pred1))
 print 'The accuracy is:', accuracy_score(labels_test, pred1)
+print metrics.confusion_matrix(labels_test, pred1)
+#
+# lb = preprocessing.LabelBinarizer()
+# lb.fit(features_train)
+# print(recall_score(labels_test, pred1 ,None))
 
-print("\n\n ""Naive Bayes Algorithm Results"" ")
-clf2 = GaussianNB()
-clf2.fit(features_train, labels_train)
-pred2=clf2.predict(features_test)
-#print (classification_report(labels_test, pred2))
-print 'The accuracy is:', accuracy_score(labels_test, pred2)
+
+# print("\n\n ""Naive Bayes Algorithm Results"" ")
+# clf2 = GaussianNB()
+# clf2.fit(features_train, labels_train)
+# pred2=clf2.predict(features_test)
+# print(classification_report(labels_test, pred2))
+# print 'The accuracy is:', accuracy_score(labels_test, pred2)
+# print metrics.confusion_matrix(labels_test, pred2)
+
 
 print("\n\n ""K-Nearest Neighbours Algorithm Results"" ")
 clf3 = KNeighborsClassifier()
 clf3.fit(features_train, labels_train)
 pred3=clf3.predict(features_test)
-#print(classification_report(labels_test, pred3))
+print(classification_report(labels_test, pred3))
 print 'The accuracy is:', accuracy_score(labels_test, pred3)
+print metrics.confusion_matrix(labels_test, pred3)
 
 print("\n\n ""Random Forest Algorithm Results"" ")
-clf4 = RandomForestClassifier( min_samples_split=3)
+clf4 = RandomForestClassifier(min_samples_split=7)
 clf4.fit(features_train, labels_train)
 pred4=clf4.predict(features_test)
-#print(classification_report(labels_test, pred4))
+print(classification_report(labels_test, pred4))
 print 'The accuracy is:', accuracy_score(labels_test, pred4)
+print metrics.confusion_matrix(labels_test, pred4)
--- trial.py
+++ trial.py
@@ -40,40 +40,8 @@
 #from selenium import webdriver
 #url = 'http://www.stackoverflow.com'
 #driver = webdriver.PhantomJS()
-#driver.get(url)
+#river.get(url)
 #print driver.page_source
-
-# import whois
-# from datetime import datetime
-# import time
-# try:
-#     domain=whois.query("dbfjefklj.com")
-# except:
-#     print -1
-# creation_date=domain.creation_date
-# expiration_date=domain.expiration_date
-# #creation_date=datetime.strptime(creation_date,"%Y-%m-%d")
-# #expiration_date=datetime.strptime(expiration_date,"%Y-%m-%d")
-# #today=datetime.datetime.today().strftime('%Y-%m-%d')
-# today=time.strftime('%Y-%m-%d')
-# today=datetime.strptime(today,'%Y-%m-%d')
-# ageofdomain=abs((expiration_date - creation_date).days)
-# if ageofdomain/30<6:
-#     print -1
-# else:
-#     print 1
-#
-# registration_length=abs((expiration_date-today).days)
-#
-# if registration_length/365<=1:
-#     print -1
-# else:
-#     print 1
-# print registration_length
-# print ageofdomain
-# print creation_date
-# print expiration_date
-
 import numpy as np
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
@@ -86,63 +54,51 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-import features_extraction
-
 labels=[]
 features=[]
-file=open('/home/rohit/PycharmProjects/BE/Training.arff').read()
+file=open('/home/anand/PycharmProjects/BE/Training Dataset.arff').read()
 list=file.split('\r\n')
 #f=open('/home/janvi-jatakia/Downloads/data.txt','w')
 data=np.array(list)
+print "yaha"
+print data
 data1=[i.split(',') for i in data]
 data1=data1[0:-1]
 for i in data1:
 	labels.append(i[30])
 data1=np.array(data1)
 features=data1[:,:-1]
-features=features[:,[0,1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,22,23,24,25,27,29]]
-#print features
 features=np.array(features).astype(np.float)
 
-# features_train=features[:10000]
-# labels_train=labels[:10000]
-##### Features_train doesn't have to be different from features anymore since all the features will be used for training #####
-# features_test=features[10000:]
-# labels_test=labels[10000:]
-features_test=features_extraction.status
+features_train=features[:10000]
+labels_train=labels[:10000]
+features_test=features[10000:]
+labels_test=labels[10000:]
 
 print(" ""Support Vector Machine Results"" ")
 clf1 = svm.SVC(kernel="rbf",C=10000.0,gamma=0.1,probability=True)
-# clf1.fit(features_train,labels_train)
-clf1.fit(features, labels)
+clf1.fit(features_train,labels_train)
 pred1=clf1.predict(features_test)
 #print(classification_report(labels_test, pred1))
-# print 'The accuracy is:', accuracy_score(labels_test, pred1)
-print 'The predicted label is ', pred1
+print 'The accuracy is:', accuracy_score(labels_test, pred1)
 
 print("\n\n ""Naive Bayes Algorithm Results"" ")
 clf2 = GaussianNB()
-# clf2.fit(features_train, labels_train)
-clf2.fit(features, labels)
+clf2.fit(features_train, labels_train)
 pred2=clf2.predict(features_test)
 #print(classification_report(labels_test, pred2))
-# print 'The accuracy is:', accuracy_score(labels_test, pred2)
-print 'The predicted label is ', pred2
+print 'The accuracy is:', accuracy_score(labels_test, pred2)
 
 print("\n\n ""K-Nearest Neighbours Algorithm Results"" ")
 clf3 = KNeighborsClassifier()
-# clf3.fit(features_train, labels_train)
-clf3.fit(features, labels)
+clf3.fit(features_train, labels_train)
 pred3=clf3.predict(features_test)
 #print(classification_report(labels_test, pred3))
-# print 'The accuracy is:', accuracy_score(labels_test, pred3)
-print 'The predicted label is ', pred3
+print 'The accuracy is:', accuracy_score(labels_test, pred3)
 
 print("\n\n ""Random Forest Algorithm Results"" ")
 clf4 = RandomForestClassifier( min_samples_split=3)
-# clf4.fit(features_train, labels_train)
-clf4.fit(features, labels)
+clf4.fit(features_train, labels_train)
 pred4=clf4.predict(features_test)
 #print(classification_report(labels_test, pred4))
-# print 'The accuracy is:', accuracy_score(labels_test, pred4)
-print 'The predicted label is ', pred4
+print 'The accuracy is:', accuracy_score(labels_test, pred4)
