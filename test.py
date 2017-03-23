03dafe59502e3311105f8a7af831fd1850d05243
rohit <rohit.naik246@gmail.com>
2017-03-23 15:44:52 +0530

    argument url

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-import joblib, features_extraction
-url="https://www.google.com"
+import joblib, features_extraction, sys
+url=sys.argv[0]
 features_test=features_extraction.main(url)
 
 clf = joblib.load('classifier/random_forest.pkl')
 
 pred=clf.predict(features_test)
 prob=clf.predict_proba(features_test)
-print 'The predicted probability is - ', prob
+#print 'The predicted probability is - ', prob
 print 'The predicted label is - ', pred
