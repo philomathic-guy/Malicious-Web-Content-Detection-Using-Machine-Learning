b8ec0f17ae746380d04867223a27f237b8392a9e
Rohit Naik <rohit.naik246@gmail.com>
2019-02-05 23:04:22 +0530

    Cleaned up and reformatted popup.js

--- Extension/popup.js
+++ Extension/popup.js
@@ -2,27 +2,18 @@ function transfer(){
 	var tablink;
 	chrome.tabs.getSelected(null,function(tab) {
 	   	tablink = tab.url;
-		//alert(tablink);
 		$("#p1").text("The URL being tested is - "+tablink);
 
-	var xhr=new XMLHttpRequest();
-	//alert("hii");
-	//var tablink='';	
-	//var safe = document.URL;
-	params="url="+tablink;
-	alert(params);
-	var markup = "url="+tablink+"&html="+document.documentElement.innerHTML;
-	xhr.open("POST","http://localhost/BE/clientServer.php",false);
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhr.send(markup);
-	alert(xhr.responseText);
-	$("#div1").text(xhr.responseText);	
-	return xhr.responseText;
-	/*if(xhr.responseText==6)
-	{
-		return "safe";
-	}
-	return "nooooo";*/
+		var xhr=new XMLHttpRequest();
+		params="url="+tablink;
+		alert(params);
+		var markup = "url="+tablink+"&html="+document.documentElement.innerHTML;
+		xhr.open("POST","http://localhost/Malicious-Web-Content-Detection-Using-Machine-Learning/clientServer.php",false);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(markup);
+		alert(xhr.responseText);
+		$("#div1").text(xhr.responseText);
+		return xhr.responseText;
 	});
 }
 
@@ -30,12 +21,10 @@ function transfer(){
 $(document).ready(function(){
     $("button").click(function(){	
 	var val = transfer();
-//	$("#p1").text(val);
         });
 });
 
 chrome.tabs.getSelected(null,function(tab) {
    	var tablink = tab.url;
 	$("#p1").text("The URL being tested is - "+tablink);
-
 });
