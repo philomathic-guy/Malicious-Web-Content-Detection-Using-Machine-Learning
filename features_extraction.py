8203ed18b9124d172dd873203f9370a9738730cc
Rohit Naik <rohit.naik246@gmail.com>
2019-05-10 20:10:20 -0700

    Code restructured.

--- features_extraction.py
+++ features_extraction.py
@@ -307,12 +307,12 @@ def main(url):
     hostname = url
     h = [(x.start(0), x.end(0)) for x in re.finditer('https://|http://|www.|https://www.|http://www.', hostname)]
     z = int(len(h))
-    if z != 0:
+    if not z:
         y = h[0][1]
         hostname = hostname[y:]
         h = [(x.start(0), x.end(0)) for x in re.finditer('/', hostname)]
         z = int(len(h))
-        if z != 0:
+        if not z:
             hostname = hostname[:h[0][0]]
 
     # print("Hostname is - " + hostname)
