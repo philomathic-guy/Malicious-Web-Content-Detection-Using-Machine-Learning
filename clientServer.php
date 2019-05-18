b609030043f0bfff036cbc7c87fd20c65641a0db
Rohit Naik <rohit.naik246@gmail.com>
2019-05-18 12:44:39 -0700

    Explanatory comment for python path added.

--- clientServer.php
+++ clientServer.php
@@ -10,6 +10,7 @@ $bytes=file_put_contents('markup.txt', $html);
 //$python_path=exec("which python 2>&1 ");
 //$decision=exec("$python_path test.py $site 2>&1 ");
 
+// Replace the path with the path of your python2.x installation.
 $decision=exec("/Library/Frameworks/Python.framework/Versions/2.7/bin/python2 test.py $site 2>&1 ");
 echo $decision;
 ?>
