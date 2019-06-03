ab571d13988bc2f03d4304f6c825f933daaa5ff1
Rohit Naik <rohit.naik246@gmail.com>
2019-06-02 23:33:03 -0700

    Started implementing patterns in features_extraction.

--- features_extraction.py
+++ features_extraction.py
@@ -18,16 +18,15 @@ from googlesearch import search
 # This import is needed only when you run this file in isolation.
 import sys
 
+from patterns import *
+
 # Path of your local server. Different for different OSs.
 LOCALHOST_PATH = "/Library/WebServer/Documents/"
 
 
 def having_ip_address(url):
-    match = re.search(
-        '(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.'
-        '([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\/)|'  # IPv4
-        '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)'  # IPv4 in hexadecimal
-        '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}', url)  # Ipv6
+    ip_address_pattern = ipv4_pattern + "|" + ipv6_pattern
+    match = re.search(ip_address_pattern, url)
     return -1 if match else 1
 
 
@@ -40,15 +39,7 @@ def url_length(url):
 
 
 def shortening_service(url):
-    match = re.search('bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
-                      'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
-                      'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
-                      'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
-                      'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
-                      'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
-                      'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|'
-                      'tr\.im|link\.zip\.net',
-                      url)
+    match = re.search(shortening_services, url)
     return -1 if match else 1
 
 
