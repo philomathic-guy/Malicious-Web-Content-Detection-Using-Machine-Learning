7b829f4c4aeebe2db5e0dc05e36953e88479b0c1
Rohit Naik <rohit.naik246@gmail.com>
2019-06-25 17:04:07 -0700

    Added purpose of the file.

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+# Purpose - Receive the call for testing a page from the Chrome extension and return the result (SAFE/PHISHING)
+# for display. This file calls all the different components of the project (The ML model, features_extraction) and
+# consolidates the result.
+
 import joblib
 import features_extraction
 import sys
@@ -20,17 +24,17 @@ def get_prediction_from_url(test_url):
 def main():
     url = sys.argv[1]
 
-    pred = get_prediction_from_url(url)
+    prediction = get_prediction_from_url(url)
 
     # Print the probability of prediction (if needed)
     # prob = clf.predict_proba(features_test)
     # print 'Features=', features_test, 'The predicted probability is - ', prob, 'The predicted label is - ', pred
     #    print "The probability of this site being a phishing website is ", features_test[0]*100, "%"
 
-    if pred == 1:
+    if prediction == 1:
         # print "The website is safe to browse"
         print("SAFE")
-    elif pred == -1:
+    elif prediction == -1:
         # print "The website has phishing features. DO NOT VISIT!"
         print("PHISHING")
 
