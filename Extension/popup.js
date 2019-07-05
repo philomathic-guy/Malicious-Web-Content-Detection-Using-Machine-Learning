d34b7ddd1a08fc05ca7b943df5c19dc008134eed
Rohit Naik <18022447+philomathic-guy@users.noreply.github.com>
2019-07-04 20:17:28 -0700

    Added purpose.

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,3 +1,4 @@
+// Purpose - This file contains all the logic relevant to the extension such as getting the URL, calling the server side clientServer.php which then calls the core logic.
 function transfer(){	
 	var tablink;
 	chrome.tabs.getSelected(null,function(tab) {
