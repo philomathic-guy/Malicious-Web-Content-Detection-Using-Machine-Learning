cf83985851e67e61e7199fe760af6b80ec5fcb62
Rohit Naik <rohit.naik246@gmail.com>
2019-07-07 22:55:55 -0700

    Comment for debugging added.

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,4 +1,5 @@
 // Purpose - This file contains all the logic relevant to the extension such as getting the URL, calling the server side clientServer.php which then calls the core logic.
+
 function transfer(){	
 	var tablink;
 	chrome.tabs.getSelected(null,function(tab) {
@@ -12,6 +13,7 @@ function transfer(){
 		xhr.open("POST","http://localhost/Malicious-Web-Content-Detection-Using-Machine-Learning/clientServer.php",false);
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		xhr.send(markup);
+		// Uncomment this line if you see some error on the extension to see the full error message for debugging.
 		// alert(xhr.responseText);
 		$("#div1").text(xhr.responseText);
 		return xhr.responseText;
